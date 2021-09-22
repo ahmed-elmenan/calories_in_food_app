93c5356482222f0286c660d8f505ee1fde3f6130
yabakhar <youssefabkhar52@gmail.com>
2021-09-23 00:56:56 +0100

    change images of foodCategories

--- lib/core/consts/food_categories.dart
+++ lib/core/consts/food_categories.dart
@@ -1,54 +1,141 @@
 const FOOD_CATEGORIES = [
-  {"name": "Vegetables", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Fruits", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Juices", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Viande", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Chicken", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Fish", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Milk", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Soft Drink", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Noodels", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Oils", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Cheese", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Cake", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Sausage", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Fast Food", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Mcdonald Burgers", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Mcdonald Salads", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Mcdonald Sauces", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Mcdonald Cookies", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Mcdonald Fries", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Mcdonald Cakes", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Mcdonald Ice Cream", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Mcdonald Coffee", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Burger King Burgers", "imagePath": "assets/images/vegetables.jpg"},
+  {
+    "name": "Vegetables",
+    "imagePath": "assets/images/foodCategorieImages/vegetables.png"
+  },
+  {"name": "Fruits", "imagePath": "assets/images/foodCategorieImages/oils.png"},
+  {
+    "name": "Juices",
+    "imagePath": "assets/images/foodCategorieImages/juices.png"
+  },
+  {"name": "Viande", "imagePath": "assets/images/foodCategorieImages/meat.png"},
+  {
+    "name": "Chicken",
+    "imagePath": "assets/images/foodCategorieImages/chicken.png"
+  },
+  {"name": "Fish", "imagePath": "assets/images/foodCategorieImages/fish.png"},
+  {"name": "Milk", "imagePath": "assets/images/foodCategorieImages/milk.png"},
+  {
+    "name": "Soft Drink",
+    "imagePath": "assets/images/foodCategorieImages/Soft Drink.png"
+  },
+  {
+    "name": "Noodels",
+    "imagePath": "assets/images/foodCategorieImages/noodels.png"
+  },
+  {"name": "Oils", "imagePath": "assets/images/foodCategorieImages/oils.png"},
+  {"name": "Cheese", "imagePath": "assets/images/foodCategorieImages/oils.png"},
+  {"name": "Cake", "imagePath": "assets/images/foodCategorieImages/oils.png"},
+  {
+    "name": "Sausage",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Fast Food",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Mcdonald Burgers",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Mcdonald Salads",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Mcdonald Sauces",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Mcdonald Cookies",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Mcdonald Fries",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Mcdonald Cakes",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Mcdonald Ice Cream",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Mcdonald Coffee",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Burger King Burgers",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
   {
     "name": "Burger King Cream Glace",
-    "imagePath": "assets/images/vegetables.jpg"
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
   },
-  {"name": "Burger King Dessert", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Burger King Fries", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Burger King Fruits", "imagePath": "assets/images/vegetables.jpg"},
+  {
+    "name": "Burger King Dessert",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Burger King Fries",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Burger King Fruits",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
   {
     "name": "Burger King Ice Cream",
-    "imagePath": "assets/images/vegetables.jpg"
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
   },
   {
     "name": "Burger King Salad Cesar",
-    "imagePath": "assets/images/vegetables.jpg"
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
   },
-  {"name": "Burger King Sandwich", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Burger King Glace", "imagePath": "assets/images/vegetables.jpg"},
+  {
+    "name": "Burger King Sandwich",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Burger King Glace",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
   {
     "name": "Burger King Milk Shake",
-    "imagePath": "assets/images/vegetables.jpg"
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
   },
-  {"name": "Burger King Nuggets", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Burger King Sauce", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Burger King Wraps", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Burger King Beignet", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Dominos Pizza", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Pizza Hut", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Star Bucks", "imagePath": "assets/images/vegetables.jpg"},
-  {"name": "Sub Way", "imagePath": "assets/images/vegetables.jpg"},
+  {
+    "name": "Burger King Nuggets",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Burger King Sauce",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Burger King Wraps",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Burger King Beignet",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Dominos Pizza",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Pizza Hut",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Star Bucks",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
+  {
+    "name": "Sub Way",
+    "imagePath": "assets/images/foodCategorieImages/oils.png"
+  },
 ];
