27fd9f4a28402cc417b74778c36e5ff13d106a97
ahel-men <ahel-men@student.1337.ma>
2021-10-22 18:14:11 -0700

    setup class entity and architecture folders

---
+++ lib/features/supplements/domain/entities/supplements_model.dart
@@ -0,0 +1,17 @@
+import 'package:meta/meta.dart';
+
+class Supplements {
+  final String name;
+  final String id;
+  final String imageLink;
+  final String flavor;
+  final String ingredients;
+  final String description;
+  final String serving; // scoop
+  final double proteins;
+  final double carbs;
+  final double price;
+
+  Supplements({@required this.name, @required this.id, @required this.imageLink, @required this.flavor, @required this.ingredients, @required this.description, @required this.serving, @required this.proteins, @required this.carbs, @required this.price});
+
+}
